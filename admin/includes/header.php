7396013056f210c5ee4cb52438f6827d9cd5469b
pushpak <pushpakbhavsar101@gmail.com>
2024-07-31 18:26:17 +0530

    file add

---
+++ admin/includes/header.php
@@ -0,0 +1,65 @@
+   <?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+?>
+   <header class="header dark-bg">
+      <div class="toggle-nav">
+        <div class="icon-reorder tooltips" data-original-title="Toggle Navigation" data-placement="bottom"><i class="icon_menu"></i></div>
+      </div>
+<?php
+$adid=$_SESSION['agmsaid'];
+$ret=mysqli_query($con,"select AdminName from tbladmin where ID='$adid'");
+$row=mysqli_fetch_array($ret);
+$name=$row['AdminName'];
+
+?>
+      <!--logo start-->
+      <a href="dashboard.php" class="logo"><span class="lite"><strong> ART GALLERY | Admin</strong></span></a>
+      <!--logo end-->
+
+      
+
+      <div class="top-nav notification-row">
+        <!-- notificatoin dropdown start-->
+        <ul class="nav pull-right top-menu">
+
+          
+         
+          <!-- alert notification start-->
+
+            
+             
+              
+          
+          <!-- alert notification end-->
+          <!-- user login dropdown start-->
+          <li class="dropdown">
+            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                            <span class="profile-ava">
+                                <img alt="" src="images/av1.jpg" width="40" height="30">
+                            </span>
+                            
+                            <span class="username"><?php echo $name; ?></span>
+                            <b class="caret"></b>
+                        </a>
+            <ul class="dropdown-menu extended logout">
+              <div class="log-arrow-up"></div>
+              <li class="eborder-top">
+                <a href="admin-profile.php"><i class="icon_profile"></i> My Profile</a>
+              </li>
+              <li>
+                <a href="change-password.php"><i class="icon_mail_alt"></i> Change Password</a>
+              </li>
+              
+              <li>
+                <a href="logout.php"><i class="icon_key_alt"></i> Log Out</a>
+              </li>
+             
+            </ul>
+          </li>
+          <!-- user login dropdown end -->
+        </ul>
+        <!-- notificatoin dropdown end-->
+      </div>
+    </header>
---
+++ admin/includes/sidebar.php
@@ -0,0 +1,65 @@
+<aside>
+      <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu">
+   
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_desktop"></i>
+                          <span>Artist</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="add-artist.php">Add Artist</a></li>
+              <li><a class="" href="manage-artist.php">Manage Artist</a></li>
+             
+            </ul>
+          </li>
+                <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Art Type</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="add-art-type.php">Add Type</a></li>
+              <li><a class="" href="manage-art-type.php">Manage Type</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Medium of Art</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="add-art-medium.php">Add Medium</a></li>
+              <li><a class="" href="manage-art-medium.php">Manage Medium</a></li>
+            </ul>
+          </li>
+ <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_document_alt"></i>
+                          <span>Art Products</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="add-art-product.php">Add Product</a></li>
+              <li><a class="" href="manage-art-product.php">Manage Product</a></li>
+            </ul>
+          </li>
+          <li class="sub-menu">
+            <a href="javascript:;" class="">
+                          <i class="icon_table"></i>
+                          <span>Enquiry</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+            <ul class="sub">
+              <li><a class="" href="unanswer-enquiry.php">Unanswer Enquiry</a></li>
+              <li><a class="" href="answer-enquiry.php">Answered Enquiry</a></li>
+            </ul>
+          </li>
+
+        <!-- sidebar menu end-->
+      </div>
+    </aside>
